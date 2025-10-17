d56ff122556c71cf81f4b47cc75de9bd6ea7f55d
Nikhil <kumarnikhil48578@gmail.com>
2025-10-17 21:19:35 +0530

    feat: reflexion pattern

---
+++ reflexion/src/model.ts
@@ -0,0 +1,6 @@
+import { ChatOpenAI } from "@langchain/openai";
+
+export const llm = new ChatOpenAI({
+    model: 'gpt-5-nano-2025-08-07',
+});
+
--- src/model.ts
+++ src/model.ts
@@ -3,6 +3,7 @@ import { ChatGroq } from '@langchain/groq';
 /**
  * Initilise the LLM
  */
+
 export const model = new ChatGroq({
     model: 'openai/gpt-oss-120b',
     temperature: 0,
